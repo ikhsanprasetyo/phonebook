12ef0cef18e19b486d29bebf37891e2d573a36e9
ikhsanprasetyo <ikhsan_prasetyo94@yahoo.co.id>
2023-12-08 08:21:07 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+
+
+
+/*
 import 'package:flutter/material.dart';
 import 'package:flutter_mongo/screens/home.dart';
 
@@ -5,7 +9,7 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+  class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
@@ -20,3 +24,63 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+*/
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String apiUrl = "http://localhost:3000/insert";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MongoDB Insert Example"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            _postData();
+          },
+          child: Text("Insert Data"),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _postData() async {
+    try {
+      final dio = Dio();
+
+      final response = await dio.post(
+        apiUrl,
+        data: {
+          'email': 'example@email.com',
+          'password': 'yourpassword',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        print("Data inserted successfully");
+      } else {
+        print("Failed to insert data. Error: ${response.statusMessage}");
+      }
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
+}
+
